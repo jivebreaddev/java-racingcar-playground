268c0a1a824d4e0a1dfd3895eec531417bb574c9
Jivebreaddev <jivebreaddev@gmail.com>
2023-06-18 15:50:51 +0900

    Feature:(Test,Domain) car static constructor test with illegal arguments: null, empty String, five words or more

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,5 +1,40 @@
 package racing.domain;
 
-public class Car {
+import java.util.Objects;
+import org.apache.commons.lang3.StringUtils;
 
+
+public class Car {
+  String carName;
+  private Car(String carName) {
+    this.carName = carName;
+  }
+
+  public static Car of(String carName){
+    if(!Car.isValid(carName)){
+      throw new IllegalArgumentException("자동차의 이름값을 확인해주세요");
+    }
+    return new Car(carName);
+  }
+
+  public static boolean isValid(String carName){
+    return !StringUtils.isBlank(carName) && carName.length() <= 5;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Car car = (Car) o;
+    return Objects.equals(carName, car.carName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(carName);
+  }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,7 +1,9 @@
 package racing.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,26 +11,39 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
 
+  public static final String CAR_NAME = "five";
+  public static final String ILLEGAL_NAME = "fiveguys";
+
   @Test
-  @DisplayName("자동차 이름은 5글자 이하")
-  void CarName(){
+  @DisplayName("자동차 이름은 5글자 이하일때 생성된다.")
+  void CarNameLessThanFive(){
     //GIVEN
+
     //WHEN
+    Car car = Car.of(CAR_NAME);
     //THEN
+
+    assertThat(car.equals(Car.of(CAR_NAME))).isEqualTo(true);
   }
   @Test
-  @DisplayName("자동차 이름 쉼표로 나눠진다.")
-  void CarSplitNames(){
+  @DisplayName("자동차 이름은 5글자 초과일때 오류를 던진다")
+  void CarName(){
     //GIVEN
+
     //WHEN
+    Throwable thrown = catchThrowable(() -> Car.of(ILLEGAL_NAME));
     //THEN
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
   @DisplayName("랜덤 값이 4이 상인 경우 자동차는 전진한다.")
   void CarMove(){
     //GIVEN
+    Car car = Car.of(CAR_NAME);
+
     //WHEN
+
     //THEN
   }
 
